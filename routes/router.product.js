81948707f0fd05bbc633a8779f7ba68192ae203e
FLXwkg <ewenheas13@gmail.com>
2025-01-10 14:58:57 +0100

    i

--- routes/router.product.js
+++ routes/router.product.js
@@ -28,30 +28,7 @@ const upload = multer({
  * List products
  */
 router.get('/', async (req, res) => {
-<<<<<<< HEAD
     let products = await ProductService.listProducts(req.query.sort , req.query.order , req.query.filters , req.query.limit , req.query.page);
-=======
-    let products = await ProductService.listProducts({ ...req.query });
-    res.json(products);
-})
-
-router.post('/:id', async (req, res) => {
-    let id = req.params.id;
-    let products = await ProductService.createProduct(id);
-    res.json(products);
-})
-
-router.put('/:id', async (req, res) => {
-    let id = req.params.id;
-    let params = req.body
-    let products = await ProductService.updateProduct(id, params);
-    res.json(products);
-})
-
-router.delete('/:id', async (req, res) => {
-    let id = req.params.id;
-    let products = await ProductService.deleteProduct(id);
->>>>>>> 442546da71c5005e54e372913ddfbb5689ac80d7
     res.json(products);
 })
 
